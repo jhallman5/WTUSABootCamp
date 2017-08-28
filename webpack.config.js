0e73ff610b5c5885567f2e3cf48c066670c39da6
John <jhallman5@gmail.com>
2017-08-28 12:54:13 -0700

    Gets webpack and react to work together and render /

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,35 @@
-const WebpackNotifierPlugin = require('webpack-notifier')
-module.exports = {
-  entry: './src/views/index.html',
-  output: {
-    filename: 'bundle.js'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: '/node_modules/',
-        use:[{
-          loader:'babel-loader',
-          options: {
-            presets: ['es2015', 'react']
-          }
-        }]
-      }
-    ]
-  },
-  plugins: [
-    new WebpackNotifierPlugin()
-  ]
-}
+const WebpackNotifierPlugin = require('webpack-notifier');
+
+module.exports = options => {
+  return {
+    entry: './src/views/index.js',
+    output: {
+      filename: 'bundle.js',
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: '/node_modules/',
+          use: [
+            {
+              loader: 'babel-loader',
+              options: {
+                presets: ['es2015', 'react'],
+              },
+            },
+          ],
+        },
+        {
+          test: /\.css/,
+          use: [
+            'style-loader'
+          ]
+        }
+      ],
+    },
+    plugins: [
+      new WebpackNotifierPlugin(),
+    ],
+  }
+};
