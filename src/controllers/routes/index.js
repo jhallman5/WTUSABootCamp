c79b5660363735b6ee5f2b33fafdf41126753200
John <jhallman5@gmail.com>
2017-08-29 16:49:32 -0700

    Adds CORS

--- src/controllers/routes/index.js
+++ src/controllers/routes/index.js
@@ -1,9 +1,8 @@
 const router = require('express').Router()
 
 router.get('/dashboard/john', (req, res) => {
-  console.log( "(>'')>  in Route" )
   const user = { id: 1, username: 'jhallman5'}
-  res.json(user)
+  res.set({'Access-Control-Allow-Origin': 'http://localhost:8080'}).json(user)
 })
 
 module.exports = router;
--- src/server.js
+++ src/server.js
@@ -7,7 +7,6 @@ const PORT = process.env.PORT || 3000;
 // bodyparser
 // session
 // passport
-// router
 
 server.use(router);
 
