124c70fbd6876a1ccc1e69183e6531814eb85a28
John <jhallman5@gmail.com>
2017-08-30 10:12:13 -0700

    Moves CORS header to act as middleware

--- src/controllers/routes/index.js
+++ src/controllers/routes/index.js
@@ -1,8 +1,13 @@
 const router = require('express').Router()
 
+router.use((req, res, next) => {
+  res.set({'Access-Control-Allow-Origin': 'http://localhost:8080'})
+  next()
+})
+
 router.get('/dashboard/john', (req, res) => {
   const user = { id: 1, username: 'jhallman5'}
-  res.set({'Access-Control-Allow-Origin': 'http://localhost:8080'}).json(user)
+  res.json(user)
 })
 
 module.exports = router;
