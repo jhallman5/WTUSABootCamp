4be8118501917eb7a42b72b8026c292bd32d15cf
John <jhallman5@gmail.com>
2017-08-30 16:40:47 -0700

    Adds tabSelect, and all conditional

--- src/views/containers/DashBoard.js
+++ src/views/containers/DashBoard.js
@@ -20,33 +20,14 @@ export default class DashBoardContainer extends React.Component {
     this.state =  {
       tab: 'open'
     }
-
-  }
-  fetchUser() {
-    this.props.dispatch(Customers.fetchCustomer())
-    this.setState({tab: 'open'})
   }
 
-  //mock test
-  fetchAll() {
+  componentDidMount(){
     this.props.dispatch(Customers.fetchAll())
   }
 
-  //mock test
-  fetchJoinedUsers(){
-    this.props.dispatch(Customers.fetchSteven())
-    this.setState({tab: 'joined'})
-  }
-
-  //mock test
-  fetchRejectedUsers(){
-    this.props.dispatch(Customers.fetchLisa())
-    this.setState({tab: 'rejected'})
-  }
-
-  //mock test
-  fetchJohn(){
-    this.props.dispatch(Customers.fetchJohn())
+  tabSelect(tab) {
+    this.setState({ tab })
   }
 
   render() {
@@ -66,15 +47,13 @@ export default class DashBoardContainer extends React.Component {
         <div className="tabs is-offset-1">
           <ul>
             <li className={this.state.tab === 'open' ? "is-active" : ""}>
-              <a onClick={this.fetchUser.bind(this)}>Open</a></li>
+              <a onClick={this.tabSelect.bind(this,'open')}>Open</a></li>
             <li className={this.state.tab === 'joined' ? "is-active": ""}>
-              <a onClick={this.fetchJoinedUsers.bind(this)}>Joined</a></li>
+              <a onClick={this.tabSelect.bind(this,'joined')}>Joined</a></li>
             <li className={this.state.tab === 'rejected' ? "is-active": ""}>
-              <a onClick={this.fetchRejectedUsers.bind(this)}>Rejected</a></li>
+              <a onClick={this.tabSelect.bind(this,'rejected')}>Rejected</a></li>
             <li>
-              <a onClick={this.fetchJohn.bind(this)}>john</a></li>
-            <li>
-              <a onClick={this.fetchAll.bind(this)}>Show All</a></li>
+              <a onClick={this.tabSelect.bind(this,'all')}>All</a></li>
           </ul>
         </div>
 
@@ -86,7 +65,7 @@ export default class DashBoardContainer extends React.Component {
               <CustomerHeader />
               {this.props.customers
                 ? this.props.customers.map(person => {
-                    if(person.status === this.state.tab) {
+                    if(this.state.tab === person.status || this.state.tab === 'all') {
                       return <CustomerInfo customer={person} key={person.id}/>
                     }
                   })
