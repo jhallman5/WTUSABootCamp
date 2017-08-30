d066309b35428a52bd70ff7e466fd026d5cca48e
John <jhallman5@gmail.com>
2017-08-29 17:26:37 -0700

    Adds create:db and drop:db scripts

---
+++ src/models/db/knexfile.js
@@ -0,0 +1,42 @@
+// Update with your config settings.
+const connectionString = process.env.CONNECTION_STRING || "postgres://jhallman5@localhost:5432/WTUSABootCamp"
+module.exports = {
+
+  development: {
+    client: 'postgresql',
+    connection: connectionString
+  },
+
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};
