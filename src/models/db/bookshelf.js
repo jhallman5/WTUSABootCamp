6b369bda45e96a1cb59eec2b794fa53b42d52616
John <jhallman5@gmail.com>
2017-08-30 10:42:32 -0700

    Adds bookshelf user and customer models

--- src/models/db/bookshelf.js
+++ src/models/db/bookshelf.js
@@ -1,8 +1,12 @@
 const knex = require('knex')(require('./knexfile'))
 const bookshelf = require('bookshelf')
 
-// const User = bookshelf.Model.extend({
-//   tableName:
-// })
+const User = bookshelf.Model.extend({
+  tableName: 'users'
+})
+
+const Customer = bookshelf.Model.extend({
+  tableName: 'customers'
+})
 
 module.exports = bookshelf
