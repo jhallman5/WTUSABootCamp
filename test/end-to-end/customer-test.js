203b87534297ef1042df93a626b4e42e9d9062ec
John <jhallman5@gmail.com>
2017-09-11 16:20:06 -0700

    Adds end-to-end testing for customer and fallback routes

---
+++ test/end-to-end/customer-test.js
@@ -0,0 +1,40 @@
+/* eslint-env mocha */
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+
+const { expect } = chai;
+const url = 'http://localhost:3000';
+
+chai.use(chaiHttp);
+
+describe('Customer End-to-End', () => {
+  context('/api/dashboard/users/all', () => {
+    it('fetches has a status code of 200', (done) => {
+      chai.request(url)
+        .get('/api/dashboard/users/all')
+        .then((res) => {
+          expect(res).to.have.status(200);
+          done();
+        });
+    });
+    it('returns an array of customers', (done) => {
+      chai.request(url)
+        .get('/api/dashboard/users/all')
+        .then((res) => {
+          expect(res.body).to.be.an('array');
+          expect(res.body).to.have.length(10);
+          done();
+        });
+    });
+    it('the array elements are customers', (done) => {
+      chai.request(url)
+        .get('/api/dashboard/users/all')
+        .then((res) => {
+          expect(res.body[0].id).to.equal(1);
+          expect(res.body[1].email).to.equal('steven4@gmail.com');
+          expect(res.body[2].name).to.equal('lisa3');
+          done();
+        });
+    });
+  });
+});
--- test/integration/custumer-test.js
+++ test/integration/custumer-test.js
@@ -1,21 +1,22 @@
+/* eslint-env mocha */
 const { expect } = require('chai');
-const Customer = require('../../src/models/customers')
+const Customer = require('../../src/models/customers');
 
 describe('Customer Integration', () => {
   context('findById()', () => {
     it('returns an object', (done) => {
       Customer.findById(2)
-        .then((response) => {
-          expect(response.attributes).to.be.an('object');
+        .then((res) => {
+          expect(res.attributes).to.be.an('object');
           done();
         });
     });
     it('fetches the corrected Customer', (done) => {
       Customer.findById(2)
-        .then((response) => {
-          expect(response.attributes.id).to.equal(2);
-          expect(response.attributes.name).to.equal('steven4');
-          expect(response.attributes.email).to.equal('steven4@gmail.com');
+        .then((res) => {
+          expect(res.attributes.id).to.equal(2);
+          expect(res.attributes.name).to.equal('steven4');
+          expect(res.attributes.email).to.equal('steven4@gmail.com');
           done();
         });
     });
@@ -23,18 +24,18 @@ describe('Customer Integration', () => {
   context('findAll()', () => {
     it('returns and array', (done) => {
       Customer.findAll()
-        .then((response) => {
-          expect(response.models).to.be.an('array');
-          expect(response.models.length).to.equal(10);
-          done()
-        })
+        .then((res) => {
+          expect(res.models).to.be.an('array');
+          expect(res.models.length).to.equal(10);
+          done();
+        });
     });
     it('Array elements are customers', (done) => {
       Customer.findAll()
-        .then((response) => {
-          expect(response.models[0].attributes.id).to.equal(1);
-          expect(response.models[1].attributes.email).to.equal('steven4@gmail.com');
-          expect(response.models[2].attributes.name).to.equal('lisa3');
+        .then((res) => {
+          expect(res.models[0].attributes.id).to.equal(1);
+          expect(res.models[1].attributes.email).to.equal('steven4@gmail.com');
+          expect(res.models[2].attributes.name).to.equal('lisa3');
           done();
         });
     });
