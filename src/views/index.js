2d9401632fea476d87747cf24b94d6c1b318be3b
John <jhallman5@gmail.com>
2017-09-12 12:06:47 -0700

    Removes linting errors from index.js

--- src/views/index.js
+++ src/views/index.js
@@ -1,3 +1,4 @@
+/* global document */
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Route } from 'react-router-dom';
@@ -7,20 +8,16 @@ import HomeContainer from './containers/home';
 import DashBoard from './containers/DashBoard';
 import store from './store';
 
-class App extends React.Component { // eslint-disable-line
-  render() {
-    return (
-      <BrowserRouter >
-        <div>
-          Project Level Up
-          <Route exact path="/" component={HomeContainer} />
-          <Route path="/dashboard" component={DashBoard} />
-        </div>
-      </BrowserRouter>
-    );
-  }
-}
+const App = () => (
+  <BrowserRouter >
+    <div>
+      Project Level Up
+      <Route exact path="/" component={HomeContainer} />
+      <Route path="/dashboard" component={DashBoard} />
+    </div>
+  </BrowserRouter>
+);
 
 ReactDOM.render(<Provider store={store}>
   <App />
-</Provider>, document.getElementById('app')); // eslint-disable-line
+</Provider>, document.getElementById('app'));
