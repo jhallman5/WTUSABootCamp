1dcf4f75bfe5e046cf63bd9429054ffbaca33efc
John <jhallman5@gmail.com>
2017-09-12 10:57:05 -0700

    Adds stage 0 to babel

--- test/UI/Dashboard.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { expect } from 'chai';
-import { shallow } from 'enzyme';
-
-import Dashboard from '../../src/views/containers/DashBoard';
-
-describe('<DashBoard />', () => {
-  it('renders')
-});
---
+++ test/UI/amountOfCustomers-test.js
@@ -0,0 +1,14 @@
+//  NOT YET OPERATIONAL
+
+// const React = require('react');
+// const { expect }= require('chai');
+// const { shallow }= require('enzyme');
+//
+// const amountOfCustomers = require('../../src/views/components/amount-of-customers');
+//
+// describe('<DashBoard />', () => {
+//   it('renders 3 <amountOfCustomers components', () => {
+//     const wrapper = shallow(<amountOfCustomers customers={} status='open' />)
+//     expect(wrapper.find('.column').to.have.length(17);
+//   });17
+// });
