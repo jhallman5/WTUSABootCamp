45a52709cc74877ec1874d5922b372368aa9e00a
John <jhallman5@gmail.com>
2017-09-12 12:01:50 -0700

    Removes linter error from home component

--- src/views/components/home.js
+++ src/views/components/home.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-export default class Home extends React.Component { // eslint-disable-line
-  render() {
-    return (
-      <div>
-        <Link to={'/dashboard'}>Dashboard</Link>
-      </div>
-    );
-  }
-}
+const Home = () => (
+  <div>
+    <Link to={'/dashboard'}>Dashboard</Link>
+  </div>
+);
+
+export default Home;
