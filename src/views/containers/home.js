21fa006f3a5dcdb1efa9261143c1b9e44379cb2c
John <jhallman5@gmail.com>
2017-09-12 12:12:13 -0700

    Fixes linting errors on home container

--- src/views/containers/home.js
+++ src/views/containers/home.js
@@ -1,13 +1,10 @@
-import React, { Component } from 'react'; // eslint-disable-line
-
+import React from 'react';
 import Home from '../components/home';
 
-export default class HomeContainer extends React.Component { // eslint-disable-line
-  render() {
-    return (
-      <div>
-        <Home />
-      </div>
-    );
-  }
-}
+const HomeContainer = () => (
+  <div>
+    <Home />
+  </div>
+);
+
+export default HomeContainer;
